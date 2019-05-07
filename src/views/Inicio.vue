ff8beed9b0f65b44d9c942e6baba97ac39aa27d1
Cristopher Q <cristopq29@gmail.com>
2019-05-07 21:10:22 +0200

    Update

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -13,11 +13,11 @@
                 <div class="card col-md-12 p-3 my-4">
                     <div class="row ">
                         <div class="col-md-4">
-                            <img class="w-100" src="https://i.travelapi.com/hotels/2000000/1460000/1452100/1452074/925e404d_b.jpg">
+                            <img class="w-100" src="https://i.imgur.com/SjEGt8D.png">
                         </div>
                         <div class="col-md-8">
                             <div class="card-block">
-                                <h6 class="card-title">Card Title</h6>
+                                <h6 class="card-title">Falta de limpieza en las calles</h6>
                                 <div class="progress progress-striped">
                                     <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
                                         <span>40 votos de 100 </span>
@@ -36,10 +36,10 @@
                         </div>
                         <div class="col-md-8">
                             <div class="card-block">
-                                <h6 class="card-title">Card Title</h6>
+                                <h6 class="card-title">Mal uso de las plazas de carga y descarga</h6>
                                 <div class="progress progress-striped">
-                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                        <span>40 votos de 100 </span>
+                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                        <span>20 votos de 100 </span>
                                     </div>
                                 </div>
                                 <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -51,14 +51,14 @@
                 <div class="card col-md-12 p-3 my-4">
                     <div class="row ">
                         <div class="col-md-4">
-                            <img class="w-100" src="https://media.grenoble-tourisme.com/filer_public_thumbnails/filer_public/5b/73/5b735f80-ae5b-44c5-93a5-99e0cf36b584/grenoble_patrimoine__pierre_jayet-3.jpg__350x350_q90_crop_subject_location-2007%2C1469_subsampling-2_upscale.jpg">
+                            <img class="w-100" src="https://ssl.viaimage.viafrance.com/img/img-350x350/2/9/3/293101_350x350.jpg">
                         </div>
                         <div class="col-md-8">
                             <div class="card-block">
-                                <h6 class="card-title">Card Title</h6>
+                                <h6 class="card-title">Zona infantil en mal estado</h6>
                                 <div class="progress progress-striped">
-                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                        <span>40 votos de 100 </span>
+                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                        <span>80 votos de 100 </span>
                                     </div>
                                 </div>
                                 <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
